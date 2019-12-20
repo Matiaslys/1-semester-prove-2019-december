13d7ebf431e793f6e435da513a82b172aeb73840
Matiaslys <mati1857@edu.easj.dk>
2019-12-20 11:25:52 +0100

    Femte opgave er lavet

--- src/Person.java
+++ src/Person.java
@@ -9,9 +9,9 @@ public class Person {
 
     // TODO Ret nedenstående datatyper
     String navn; // Test Testesen
-    String alder; // 18
+    int alder; // 18
     String by; // Næstved
-    String postNummer; // 4700
+    int postNummer; // 4700
     String vej; // Femøvej
     String husnummer; // 3B
     String nationalitet; // Dansk
@@ -19,14 +19,27 @@ public class Person {
     String smarthomeDevices; // Xbox, 3 Smartlamper, Køleskab m.m.
     String hojde; // 1.56 m
     String vaegt;  // 76 kg
-    String karaktergennemsnit; // 5.33
+    double karaktergennemsnit; // 5.33
     String eksamensKarakterer; // 7, 12, -3, 4, 2, 10
     String opdateretDato; // Sun Jan 08 00:00:00 CET 1978
 
     // TODO Vis eksempler på typecasting
     @Override
     public String toString() {
-        return "Navn: " + navn + "By" + by;
+        return "Navn: " + navn + "\n" +
+                "By: " + by + "\n" +
+                "Alder: " + alder + "\n" +
+                "Postnummer: " + postNummer + "\n" +
+                "Vej: " + vej + "\n" +
+                "Husnummer: " + husnummer + "\n" +
+                "Nationalitet: " + nationalitet + "\n" +
+                "FritidsStringeresser: " + fritidsStringeresser + "\n" +
+                "SmarthomeDevices " + smarthomeDevices + "\n" +
+                "Hojde: " + hojde + "\n" +
+                "Vaegt: " + vaegt + "\n" +
+                "Karraktergennemsnit: " + karaktergennemsnit + "\n" +
+                "EksamensKarakterer: " + eksamensKarakterer + "\n" +
+                "OpdateretDato: " + opdateretDato;
     }
 
     public static void main(String[] args) {
@@ -34,7 +47,19 @@ public class Person {
         Person person = new Person();
         person.navn = "Test Testesen";
         person.by = "Næstved";
-        System.out.println();
+        person.alder = 18;
+        person.postNummer = 4700;
+        person.vej = "Femøvej";
+        person.husnummer = "3B";
+        person.nationalitet = "Dansk";
+        person.fritidsStringeresser = "Snitning, LOL, strikke";
+        person.smarthomeDevices = "Xbox, 3 Smartlamper, Køleskab m.m.";
+        person.hojde = "1.56 m";
+        person.vaegt = "76 kg";
+        person.karaktergennemsnit = 5.33;
+        person.eksamensKarakterer = "7, 12, -3, 4, 2, 10";
+        person.opdateretDato = "Sun Jan 08 00:00:00 CET 1978";
+        System.out.println(person);
     }
 
 }
