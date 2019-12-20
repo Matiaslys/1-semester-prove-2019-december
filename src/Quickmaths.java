fd7db2b1c2e93b6d0c387d673aa91eeb893a1f7b
Matiaslys <mati1857@edu.easj.dk>
2019-12-20 09:58:29 +0100

    Tredje opgave lavet alt undtagen test den skal jeg til at lave nu.

--- src/Quickmaths.java
+++ src/Quickmaths.java
@@ -1,11 +1,41 @@
-/**
- * Developed by András Ács (acsandras@gmail.com)
- * Zealand / www.zealand.dk
- * Licensed under the MIT License
- * 13/12/2019
- */
+import java.util.Scanner;
+
 
 public class Quickmaths {
 
+    Scanner scanner = new Scanner(System.in);
+
+    double gennemsnit(double a, double b) {
+
+
+        return ((a + b) / 2);
+    }
+
+    int Sum(int a, int b) {
+
+
+        return (a+b);
+    }
+
+    public void start() {
+    while (true) {
+        Quickmaths math = new Quickmaths();
+        System.out.println("**** Quickmaths ****");
+        System.out.print("Indtast A: ");
+        int First = scanner.nextInt();
+        System.out.print("Indtast B: ");
+        int Anden = scanner.nextInt();
+        System.out.println("Sum: " + math.Sum(First,Anden));
+        System.out.println("Gennemsnit: " + math.gennemsnit(First,Anden));
+    }
+
+    }
+
+    public static void main(String[] args) {
+        Quickmaths math = new Quickmaths();
+        math.start();
+
+
+    }
 
 }
